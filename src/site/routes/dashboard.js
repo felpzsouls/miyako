6226c81785c7c3b430e6135c7c3d5950050d8ad7
felpzsouls <felipedark2509@gmail.com>
2024-07-30 00:48:19 -0300

    #2 continue...

--- src/site/routes/dashboard.js
+++ src/site/routes/dashboard.js
@@ -5,7 +5,21 @@ const { Router } = require(`express`),
 route.get(`/`, async (req, res) => {
   if (!req.isAuthenticated()) return res.redirect(`/auth/discord`)
 
-  const user = req.user;
+  const user = req.user,
+    guilds = [],
+    guild = await bot.guilds.fetch(x => {
+      const member = x.members.cache.get(user.id)
+      if (member) {
+        const perm = member.permissions.has('MANAGE_GUILD'),
+          own = x.ownerId === user.id;
+        
+          if (perm || own) {
+            guilds.push(x);
+          }
+      }
+    });
+
+    console.log(guilds)
 
   res.render(`dashboard`, {
     bot,
