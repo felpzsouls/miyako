370aaf628ceb5099a26071e09027cedd25086988
felpzsouls <felipedark2509@gmail.com>
2024-08-05 11:15:37 -0300

    #5 guilds start

--- src/site/routes/guilds.js
+++ src/site/routes/guilds.js
@@ -1,12 +1,30 @@
 const { Router } = require(`express`),
-    route = Router();
+    route = Router(),
+    bot = require(`../../bot`).bot;
+
+
+route.get(`/:id`, async (req, res) => {
+    if (!req.isAuthenticated()) return res.redirect(`/dashboard`);
+
+    const user = req.user,
+        guild = bot.guilds.cache.get(req.params.id);
+
+    if (!guild) return res.redirect(`/dashboard`);
+
+    res.render(`guilds`, {
+        user,
+        bot,
+        guild
+    })
+})
 
 route.get(`/callback`, async (req, res) => {
-    const guildId = req.query.state;
-    const code = req.query.code;
+    if (!req.isAuthenticated()) return res.redirect(`/dashboard`);
 
-    // prossiga com a lógica de autorização usando o guildId e o code
-    res.send(`guild id: ${guildId}, code: ${code}`);
+    const guildId = req.query.state;
+    if (!guildId) return res.redirect(`/dashboard`);
+
+    res.redirect(`/guilds/${guildId}`);
 })
 
 module.exports = route
